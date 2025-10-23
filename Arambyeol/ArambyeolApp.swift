ce6987ad32c4f1a7402b39b8515fa28b3453178a
DonghyeonKang <donghyeon3372@gmail.com>
2025-10-23 21:52:56 +0900

    init

---
+++ Arambyeol/ArambyeolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArambyeolApp.swift
+//  Arambyeol
+//
+//  Created by donghyeon kang on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ArambyeolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
